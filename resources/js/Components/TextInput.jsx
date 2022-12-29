80ac7e194f725de25544276ec2adaa20c8171d81
eriieii <eriiferdian92@gmail.com>
2022-12-29 13:54:54 +0700

    feat: slicing sign in & sign up

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,7 +1,35 @@
 import { forwardRef, useEffect, useRef } from 'react';
+import PropTypes from 'prop-types';
+import '../../css/textinput.css';
+
+forwardRef.propTypes = {
+    type: PropTypes.oneOf(["text", "email", "password", "number", "file"]),
+    name: PropTypes.string,
+    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+    className: PropTypes.string,
+    autoComplete: PropTypes.string,
+    required: PropTypes.bool,
+    isFocused: PropTypes.bool, 
+    handleChange: PropTypes.func,
+    placeholder: PropTypes.string, 
+    isError: PropTypes.bool
+}
 
 export default forwardRef(function TextInput(
-    { type = 'text', name, id, value, className, autoComplete, required, isFocused, handleChange },
+    { type = 'text', 
+    name, 
+    id, 
+    value, 
+    defaultValue, 
+    className,
+    autoComplete, 
+    variant = 'primary',
+    required, 
+    isFocused, 
+    handleChange, 
+    placeholder, 
+    isError, },
     ref
 ) {
     const input = ref ? ref : useRef();
@@ -19,14 +47,15 @@ export default forwardRef(function TextInput(
                 name={name}
                 id={id}
                 value={value}
+                defaultValue={defaultValue}
                 className={
-                    `border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ` +
-                    className
-                }
+                        `rounded-2xl bg-form-bg py-[13px] px-7 w-full ${isError && "input-error"} input-${variant}`
+                 }
                 ref={input}
                 autoComplete={autoComplete}
                 required={required}
                 onChange={(e) => handleChange(e)}
+                placeholder={placeholder}
             />
         </div>
     );
