64a94e0d0d7e0b283a6e68df07e8bd795b5e087b
eriieii <eriiferdian92@gmail.com>
2022-12-30 16:33:27 +0700

    feat : slicing browse Movie

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -2,6 +2,7 @@ import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from 'react-flickity-component';
 import {Head} from '@inertiajs/inertia-react';
 import FeauturedMovies from "@/Components/FeaturedMovies";
+import BrowseMovies from "@/Components/BrowseMovies";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -20,9 +21,9 @@ export default function Dashboard() {
                 href="https://unpkg.com/flickity@2/dist/flickity.min.css"
             />
         </Head>
-         <div>
+        <div>
                     <div className="font-semibold text-[22px] text-black mb-4">Featured Movies</div>
-             {/* Movie Thumbnail  */}
+                    {/* Movie Thumbnail  */}
                     <Flickity className="gap-[30px]" options={flickityOptions}>
                         {[1,2,3,4].map(i =>
                             <FeauturedMovies
@@ -34,8 +35,22 @@ export default function Dashboard() {
                                 rating={i + 1}
                             />
                         )}
-
                     </Flickity>
          </div>
+
+        <div className="mt-[50px]">
+            <div className="font-semibold text-[22px] text-black  mb-4">Browse</div>
+            <Flickity className="gap-[30px]" options={flickityOptions}>
+                {[1,2,3,4,5,6,7,8].map(i =>
+                    <BrowseMovies
+                        key={i}
+                        slug="spiderman-beginer"
+                        name={`The Spiderman Beginer ${i}`}
+                        category="Comedy"
+                        thumbnail="https://images.unsplash.com/photo-1524481905007-ea072534b820?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    />
+                )}
+            </Flickity>
+        </div>
     </Authenticated>
 }
